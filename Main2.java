240f9f847291d15df2a99e7dabbdc7370f132b11
gen.java <132185718+GrizonRussell@users.noreply.github.com>
2023-09-13 14:06:19 +0800

    Add files via upload

---
+++ Main2.java
@@ -0,0 +1,14 @@
+import java.util.LinkedList;
+
+public class Main2 {
+    public static void main(String[] args) {
+        LinkedList<String> cars = new LinkedList();
+        cars.add("BMW");
+        cars.add("Bently");
+        cars.add("Lamborghini");
+        cars.add("Ferarri");
+        cars.add("Koenigsegg");
+
+        System.out.println(cars);
+    }
+}
